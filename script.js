4148e7b5266723de89d573e28758fbec92e556ea
Mario Gamiz <mario.gamizavello@gmail.com>
2021-12-06 00:58:52 -0600

    Functionality is done, for loop used to play 5 games, and there is a counter that count who win that game. It only plays 5 games so ties counts in the final 5 games

--- script.js
+++ script.js
@@ -1,47 +1,62 @@
 
 let choices = ['rock', 'paper', 'scissors'];
-
+let winner = 0; //this variable helps to determine who won to the counter, if 1 player won if 0 computer won
+let computerCount =0;
+let playerCount=0;
+let tieCount =0;
 function computerPlay(){
     return choices[Math.floor(Math.random() * [choices.length])];
 }
 
 function playRound(computerSelection,playerSelection){
-    
+   
     if (playerSelection ===  computerSelection ){
-        alert("Its a tie");
+        console.log("Its a tie");
+        return 2;
     } else if(playerSelection === 'rock' && computerSelection ==='scissors'){
-        alert("Rock Beat Scissors, Player Won!");
+        console.log("Rock Beat Scissors, Player Won!");
+        return 1;
     } else if(playerSelection === 'rock' && computerSelection ==='paper'){
-        alert("Paper Beat Rock, Computer Won!");
+        console.log("Paper Beat Rock, Computer Won!");
+        return 0;
     } else if(playerSelection === 'paper' && computerSelection ==='rock'){
-        alert("Paper Beat Rock, Player Won!");
+        console.log("Paper Beat Rock, Player Won!");
+        return 1;
     } else if(playerSelection === 'paper' && computerSelection ==='scissors'){
-        alert("Scissors Beat Paper, Computer Won!");
+        console.log("Scissors Beat Paper, Computer Won!");
+        return 0;
     } 
     else if(playerSelection === 'scissors' && computerSelection ==='rock'){
-        alert("Rock Beat Scissors, Computer Won!");
+        console.log("Rock Beat Scissors, Computer Won!");
+        return 0;
     } else if(playerSelection === 'scissors' && computerSelection ==='paper'){
-        alert("Scissors Beat Paper, Player Won!");
+        console.log("Scissors Beat Paper, Player Won!");
+        return 1;
     } 
     
 
 }
-
-
-
-
-const computerSelection = computerPlay();
-let pick = prompt("Pick one: rock, paper or scissors?");
-const playerSelection = pick.toLowerCase();;
-
-
-
-
-
-
-
-playRound(computerSelection, playerSelection);
-
-console.log("Selection of computer:" + computerSelection);
-console.log("Player Selection:" + playerSelection);
-
+function game(){
+    for (let i = 0; i < 5; i++) {
+        console.log("game:" + (i+1));
+        const computerSelection = computerPlay();
+        let pick = prompt("Pick one: rock, paper or scissors?");
+        const playerSelection = pick.toLowerCase();
+        winner = playRound(computerSelection, playerSelection);
+        if (winner === 2){
+            tieCount = tieCount +1;
+        } else if(winner === 1){
+            playerCount = playerCount +1 ;
+        } else if (winner===0){
+            computerCount = computerCount +1;
+        }
+        
+        
+        console.log("Selection of computer:" + computerSelection);
+        console.log("Player Selection:" + playerSelection);
+        console.log("Player wons:" + playerCount);
+        console.log("Computer Won:" + computerCount);
+        console.log("Ties:" + tieCount);
+      }
+}
+game();
